d5884ee364efddfc92076f72a06e8043785c1492
rimedang <arim0930@gmail.com>
2025-04-25 13:35:50 +0900

    노트북 올림

---
+++ arim/index.js
@@ -0,0 +1,28 @@
+const todos = []; //배열만듦듦
+
+function addTodo() {
+  const input = document.getElementById('todoInput');
+  const value = input.value.trim(); //trim 공백제거메소드
+  if (value) {
+    todos.push(value);
+    input.value = '';
+    renderTodos();
+  }
+}
+
+function renderTodos() {
+  const list = document.getElementById('todoList');
+  list.innerHTML = '';
+  todos.forEach((todo, index) => {
+    //forEach를 만들어서 클릭만하면 삭제 가능하게 해줌.
+    const li = document.createElement('li');
+    li.textContent = todo;
+    li.onclick = () => removeTodo(index); //li를 클릭하면 moveTodo함수 실행
+    list.appendChild(li); //li를 list에 추가
+  });
+}
+
+function removeTodo(index) {
+  todos.splice(index, 1); //splice는 배열에서 요소를 제거하는 메소드
+  renderTodos(); //삭제하고 다시 렌더링
+}
